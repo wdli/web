5f932f3fe97647b39a712c7b701ee394784e9b7f
DavidLi <David.Li@CS535A.(none)>
2014-08-28 10:52:33 -0700

    Added a template rendering function

--- flask/test_flask.py
+++ flask/test_flask.py
@@ -3,10 +3,10 @@
 #
 # Learning Flask framework
 #
-# Update 8/25/14
+# Update 8/28/14
 #
 
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 
 #
 # Create an instance of the app
@@ -29,11 +29,11 @@ def login():
 
 
 #
-# user page
+# user page: pass a variable in <> to function
 #
 @app.route('/user/<username>')
 def profile(username):
-	return username + "'s" + " profile"
+	return '<h1>' + username + "'s" + " profile" + '</h1>'
 
 #
 # This will cause redirection with a trailing /
@@ -43,6 +43,15 @@ def projects():
         return "<h1>Projects</h1>"\
 	       "<p>Projects Page, sorry there are no projets here!, Please come back later</p>"
 
+
+# 
+# Use an existing template
+#
+@app.route('/Amazeriffic/')
+def ameri_template():
+	return render_template("Amazeriffic.html")
+
+
 #
 # This will NOT cause redirection without a trailing /
 #
@@ -51,9 +60,14 @@ def about():
 	return "<h1>About Page</h1>"
 
 
-
+#
+# Use url_for to generate URLs
+#
 with app.test_request_context():
 	print url_for('profile', username="John Doe")
+	print url_for('login')
+	print url_for('about')
+	print url_for('about',next='/what',nextnext='whatwhat')
 
 if __name__ == "__main__":
 	#app.run(debug = True)
