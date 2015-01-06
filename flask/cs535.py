9720608059b5ad13b2c4ab127d9f5b6595d9e9bc
W. David Li <wdli@wdli-lenovo.(none)>
2015-01-06 11:23:41 -0800

    Added input forms using Flask-WTF extension, not handling the refresh yet

--- flask/cs535.py
+++ flask/cs535.py
@@ -10,29 +10,72 @@
 #  flask-bootstrap
 #  flask-wtf
 #
+# Use templates: cs535.html, cs535-project2.html
+#
+# To run: ./cs535.py runserver -h 0.0.0.0 or localhost -p 6001 -r
+##################################################################
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
+#
+#Create flask app and configuration
+#
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "cs535"
 
-manager = Manager(app)
+#
+# Instantiate manager and bootstrap using the app
+#
+manager   = Manager(app)
 bootstrap = Bootstrap(app)
 
-
+#
+# Submission form class
+#
+class SubmissionForm(Form):
+    student_id = StringField("What's your NPU student ID?", validators=[Required()])
+    submit = SubmitField('Submit')
+    
+#
+# Route for '/'
+# use cs535.html template
+#
 @app.route('/')
 def index():
     return render_template('cs535.html')
-'''
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
-'''
-
-@app.route('/checkDB')
+#
+# Route for 'checkDB'
+# Use cs535_projec2.html template
+#
+@app.route('/checkDB', methods=['GET','POST'])
 def checkDB():
-    return "<h1>DB not found, please wait...!</h1>"
+    id = None
+    form = SubmissionForm()
+    if form.validate_on_submit():
+        #
+        # if validated, use redirect to handle browser refresh
+        #
+        #session['student_id'] = form.student_id.data
+        id = form.student_id.data
+        form.student_id.data = ''
+        #return redirect(url_for('checkDB'))
 
+    #
+    # If validated, then display the template with a real id
+    # If not, then display the template with an empty id
+    # and template logic will handle it properly
+    #
+    #return render_template('cs535_project2.html', form=form, name=session.get('student_id'))
+    return render_template('cs535_project2.html', form=form, student_id=id)
+
+
+#
+# Main
+#
 if __name__ == '__main__':
     manager.run()
